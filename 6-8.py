a0185974d092ae88117c03c9c7d7bd507e9a4768
mar9o0 <margoshennka@mail.ru>
2025-08-20 13:00:48 +0300

    еще одна задачка решена

---
+++ 6-8.py
@@ -0,0 +1,8 @@
+pet_0 = {'name' : 'Didi', 'categori' : 'dog', 'user' : 'Den'}
+pet_1 = {'name' : 'Bim', 'categori' : 'cat' , 'user' : 'Mila'}
+pet_2 = {'name' : 'Lari', 'categori' : 'cat', 'user' : 'Sam'}
+
+pets = [pet_0, pet_1, pet_2]
+
+for pet in pets:
+    print(pet)
