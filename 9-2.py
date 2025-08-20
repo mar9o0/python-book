2cde91aa5e4919e1d77ea396cd4cfc83176c86a6
mar9o0 <margoshennka@mail.ru>
2025-08-20 13:55:53 +0300

    задача 9-2

---
+++ 9-2.py
@@ -0,0 +1,24 @@
+class Restaurant:
+    def __init__(self, restaurant_name, c_type):
+        self.restaurant_name = restaurant_name
+        self.type = c_type
+
+    def describe_restaurant(self):
+        print("\nИмя Ресторана: " + self.restaurant_name.title() + "." )
+        print("\nТип Ресторана: " + self.type.title() + "." )
+        
+    def open_restaurant(self):
+        print("\nРесторан " + self.restaurant_name.title() + " открыт!")   
+
+my_restaurant = Restaurant('Polasko', 'Asian')
+your_restaurant = Restaurant('Kiko', 'Korean')
+his_restaurant = Restaurant('Hipt', 'Chian')
+    
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
+
+your_restaurant.describe_restaurant()
+your_restaurant.open_restaurant()
+
+his_restaurant.describe_restaurant()
+his_restaurant.open_restaurant()
