7d7a66fd35adc664ead20d40fb5b6351eb74ed7d
mar9o0 <margoshennka@mail.ru>
2025-08-20 13:53:16 +0300

    задача 8-11

---
+++ 8-11.py
@@ -0,0 +1,15 @@
+def show_magicians(names):
+    """Вывод имени каждого волшебника """
+    for name in names:
+        msg = "Hello " + name.title() + "!"
+        print(msg)
+magic = ['devid', 'josh', 'pol']
+
+show_magicians(magic[:])
+
+def make_great(magicians):
+    for i in range(len(magicians)):     # range(len(magicians)) - создает последовательность индексов
+        magicians[i] = 'Great ' + magicians[i]
+
+make_great(magic)
+show_magicians(magic)
