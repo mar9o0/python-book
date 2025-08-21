82fcb7df060b21c008add324d95f4daa0f5524fb
mar9o0 <margoshennka@mail.ru>
2025-08-21 23:17:32 +0300

    еще задача

---
+++ 9-4.py
@@ -0,0 +1,27 @@
+class Restaurant:
+    def __init__(self, restaurant_name, c_type):
+        self.restaurant_name = restaurant_name
+        self.type = c_type
+        self.number_served = 0
+
+    def restaurant(self):
+        print("Забарнировано столиков на данный момент: " + str(self.number_served))
+
+    def set_number_served(self, guests):
+        self.guests = guests
+        print("Обслужено гостей на данный момент: " + self.guests) 
+
+
+    def describe_restaurant(self):
+        print("\nИмя Ресторана: " + self.restaurant_name.title() + "." )
+        print("\nТип Ресторана: " + self.type.title() + "." )
+        
+    def open_restaurant(self):
+        print("\nРесторан " + self.restaurant_name.title() + " открыт!")   
+
+my_restaurant = Restaurant('Polasko', 'Asian')
+    
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
+my_restaurant.number_served = 23
+my_restaurant.restaurant()
