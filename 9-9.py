7a98ecc5d3925ef9c06bdd759fa15bd309fb685b
mar9o0 <margoshennka@mail.ru>
2025-08-29 15:37:47 +0300

    задача 9-9

---
+++ 9-9.py
@@ -0,0 +1,69 @@
+class Car():
+    """Простая модель автомобиля."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+ 
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class Battery():
+    """Простая модель аккумулятора электромобиля."""
+    def __init__(self, battery_size=100):
+        """Инициализирует атрибуты аккумулятора."""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумулятора."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Выводит приблизительный запас хода для аккумулятора."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+    def upgrade_batery(self):
+        """Увеличивает емкость аккумулятора до 100 кВт*ч"""
+        if self.battery_size == 100:
+            print("Размер вашего аккамулятора уже состовляет 100 кВт*ч!")
+        else:
+            self.battery_size = 100
+            print("Емкость вашего аккамулятора увеличена до 100 - кВт*ч!")
+
+
+class ElectricCar(Car):
+     """Представляет аспекты машины, специфические для электромобилей."""
+     def __init__(self, make, model, year):
+
+        """ Инициализирует атрибуты класса-родителя.
+ Затем инициализирует атрибуты, специфические для электромобиля."""
+
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_batery()
+my_tesla.battery.get_range()
